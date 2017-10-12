5a64dce0e8303552b3d61565f8addc89491f7d32
Jeremy Jones <banjeremy@gmail.com>
2017-10-12 09:33:32 -0500

    fix bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.scss';
 import 'whatwg-fetch';
 
 fetch('/api/hitCount', response => {
-  response.json();
+  return response.json();
 }).then(payload => {
   document.write(`<h1>hit count: ${payload.hitCount}</h1>`);
 });
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -44,7 +44,9 @@ module.exports = {
     ],
     port: 8080,
     host: '0.0.0.0',
-    disableHostCheck: true
+    disableHostCheck: true,
+    hot: false,
+    inline: false,
   },
   plugins: [
     new HtmlWebpackPlugin({
